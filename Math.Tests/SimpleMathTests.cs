bdf4aa8900e90b099fba785643302e39b92dcb35
PhilBroderickFS <phil.broderick@forthcom.co.uk>
2021-12-01 16:49:04 +0000

    add tests

--- Math.Tests/SimpleMathTests.cs
+++ Math.Tests/SimpleMathTests.cs
@@ -1,3 +1,4 @@
+using System;
 using SonarMathTest;
 using Xunit;
 
@@ -21,12 +22,42 @@ namespace Math.Tests
             Assert.Equal(1, result);
         }
 
-        // [Fact]
-        // public void Divide_NonZeroInput_ReturnsDivisionOfInput()
-        // {
-        //     var result = SimpleMath.Divide(4, 2);
-        //     
-        //     Assert.Equal(2, result);
-        // }
+        [Fact]
+        public void Divide_NonZeroInput_ReturnsDivisionOfInput()
+        {
+            var result = SimpleMath.Divide(4, 2);
+            
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Divide_ZeroInput_ThrowsDivideByZeroException()
+        {
+            Assert.Throws<DivideByZeroException>(() => SimpleMath.Divide(1, 0));
+        }
+
+        [Fact]
+        public void Multiply_WhenCalled_ReturnsMultiplicationOfInput()
+        {
+            var result = SimpleMath.Multiply(1, 2);
+            
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Remainder_WhenCalled_ReturnsRemainderOfInput()
+        {
+            var result = SimpleMath.Remainder(3, 2);
+            
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void Square_WhenCalled_ReturnsSquareOfInput()
+        {
+            var result = SimpleMath.Square(2);
+            
+            Assert.Equal(4, result);
+        }
     }
 }
