dc5bddf0dd59160f23971088c53ffa9b3499de89
PhilBroderickFS <phil.broderick@forthcom.co.uk>
2021-12-01 15:56:10 +0000

    add math class

---
+++ SonarMathTest/SimpleMath.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SonarMathTest
+{
+    public static class SimpleMath 
+    {
+        public static int Add (int a, int b) 
+        {
+            return a + b;
+        }
+
+        public static int Subtract (int a, int b) 
+        {
+            return a - b;
+        }
+
+        public static decimal Divide (int a, int b) 
+        {
+            if (b == 0) 
+            {
+                throw new DivideByZeroException();
+            }
+
+            return a / b;
+        }
+
+        // public decimal multiply (int a, int b) {
+        //     return a * b;
+        // }
+
+    }
+}
